85d051d7ce8e43b024079e10dab38cee677aa89e
Truong Khuu Hien Vinh <vinh@bliss-interactive.com>
2018-11-12 11:51:48 +0700

    header 30%

--- webroot/modules/header.php
+++ webroot/modules/header.php
@@ -1,1 +1,13 @@
-<header></header>
+<header>
+    <div class="container">
+        <div class="logo">
+            <a href="#"><img src="images/logo.png" /></a>
+            <a href="#"><img class="icon-social" src="images/icon-facebook.png" /></a>
+            <a href="#"><img class="icon-social" src="images/icon-instagram.png" /></a>
+            <div class="time-work">
+                Thời gian làm việc : 10h - 20h
+            </div>
+        </div>
+        <div></div>
+    </div>
+</header>
