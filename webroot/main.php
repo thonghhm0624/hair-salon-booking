2e6a50c0ed8113862a4e3e0b103042ec5e72a812
Truong Khuu Hien Vinh <vinh@bliss-interactive.com>
2018-11-16 15:01:09 +0700

    responsive

--- webroot/main.php
+++ webroot/main.php
@@ -22,6 +22,7 @@ include ('modules/footer.php');
 
 <script src="assets/js/commons.bundle.js"></script>
 <script src="assets/js/index.bundle.js"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" ></script>
 
 </body>
 </html>
--- webroot/modules/navbar.php
+++ webroot/modules/navbar.php
@@ -1,4 +1,34 @@
-<nav class="navbar navbar-expand-lg container-fluid">
+<nav class="navbar navbar-expand-lg container-fluid mobile-md-hide">
+    <div class="container">
+        <div class="collapse navbar-collapse" id="navbarNav" >
+            <ul class="navbar-nav">
+                <li class="nav-item ">
+                    <a class="nav-link" href="#">Tin Tức </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Sản Phẩm</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Giới Thiệu</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Liên hệ</a>
+                </li>
+            </ul>
+            <div class="search">
+                <form action="" name="SearchForm" method="post">
+                    <input type="text" name="searchForContent" maxlength="64" id="SearchForm" placeholder="Tìm kiếm . . ." onblur="this.value=''"/>
+                </form>
+                <div class="srch_btn" onclick="SearchForm.submit()"><img src="images/icon-search.png"></div>
+            </div>
+        </div>
+    </div>
+</nav>
+
+<nav class="navbar navbar-expand-sm navbar-dark desktop-hide mobile-md-show" style="background: #063852;">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
     <div class="container">
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
