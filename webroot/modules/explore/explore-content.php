7251710c15df6019558b9c5e0f3d2a99864daaee
Hua Hoang Minh Thong <thong@bliss-interactive.com>
2018-11-20 18:51:24 +0700

    search products + article

--- webroot/modules/explore/explore-content.php
+++ webroot/modules/explore/explore-content.php
@@ -1,3 +1,37 @@
 <div class="explore-content">
-    I AM EXPLORE CONTENT
+    <div class="search-results">
+        <h2>Tìm thấy x kết quả</h2>
+    </div>
+    <a class="row articles-explore article-item" href="#">
+        <div class="col col-md-4 col-xs-12 article-explore-img">
+            <img src="images/article/1/explore.png"/>
+        </div>
+        <div class="col col-md-8 col-xs-12 article-explore-content">
+            <h2>Cách chăm sóc tóc cho nam giới</h2>
+            <p>
+                Bài hỏi đáp nhanh dưới đây sẽ gỉai đáp nhiều thắc mắc và cho bạn biết sự thật mà có thể lâu nay bạn đang lầm tưởng về cách chăm sóc tóc cho nam giới.
+            </p>
+        </div>
+    </a>
+    <div class="row products-explore">
+        <div class="col col-md-4 col-12 product-item">
+            <div class="product-name">
+                Dầu gội X-Men For Boss
+            </div>
+            <div>&nbsp;</div>
+            <div class="product-img">
+                <img src="images/product/1/product-img.png"/>
+            </div>
+            <div>&nbsp;</div>
+            <div class="product-description">
+                Hương thơm lịch lãm, cho chàng vẻ quyến rũ khó cưỡng
+            </div>
+            <div>&nbsp;</div>
+            <a class="product-read-more">Xem thêm</a>
+        </div>
+    </div>
+    <div class="row explore-pagination">
+        <a class="results-pagination previous-results-pagination" href="#">&lt;&lt; Trang trước</a>
+        <a class="results-pagination next-results-pagination" href="#">Trang kế &gt;&gt;</a>
+    </div>
 </div>
