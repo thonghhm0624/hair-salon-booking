dd1e60319c64cde01ce66cd962bd7d19a0ee0267
Truong Khuu Hien Vinh <vinh@bliss-interactive.com>
2018-11-23 10:46:53 +0700

    account_information

---
+++ webroot/modules/page/account_information.php
@@ -0,0 +1,26 @@
+<form>
+    <div class="form-group row">
+        <label for="inputEmail3" class="col-sm-5 col-form-label">Tên</label>
+        <div class="col-sm-3">
+            <input type="name" class="form-control" id="inputEmail3" placeholder="Tên">
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="inputPassword3" class="col-sm-5 col-form-label">Tuổi</label>
+        <div class="col-sm-3">
+            <input type="age" class="form-control" id="inputPassword3" placeholder="Tuổi">
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="inputPassword3" class="col-sm-5 col-form-label">Mật khẩu mới</label>
+        <div class="col-sm-3">
+            <input type="password" class="form-control" id="inputPassword3" placeholder="Mật khẩu mới">
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="inputPassword3" class="col-sm-5 col-form-label">Nhập lại mật khẩu mới</label>
+        <div class="col-sm-3">
+            <input type="password" class="form-control" id="inputPassword3" placeholder="Nhập lại mật khẩu mới">
+        </div>
+    </div>
+</form>
